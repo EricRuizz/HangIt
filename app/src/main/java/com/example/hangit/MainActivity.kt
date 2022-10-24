1bf8808c3cb718bc24a101f84f14aee87179cfba
Eric <eric.ruiz@enti.cat>
2022-10-24 10:59:02 +0200

    Start

--- app/src/main/java/com/example/hangit/MainActivity.kt
+++ app/src/main/java/com/example/hangit/MainActivity.kt
@@ -2,8 +2,22 @@ package com.example.hangit
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.example.hangit.databinding.ActivityLoginBinding
+import com.google.android.material.snackbar.Snackbar
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityLoginBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
