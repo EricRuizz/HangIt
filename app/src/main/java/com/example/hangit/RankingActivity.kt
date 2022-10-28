1e2d11573368595a3ab67d1f3ef969ba6fdf1f06
Focalita <marionabarranco@gmail.com>
2022-10-28 10:07:12 +0200

    Added ranking files

---
+++ app/src/main/java/com/example/hangit/RankingActivity.kt
@@ -0,0 +1,8 @@
+package com.example.hangit
+
+import androidx.appcompat.app.AppCompatActivity
+import com.example.hangit.databinding.ActivityRankingBinding
+
+class RankingActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityRankingBinding
+}
