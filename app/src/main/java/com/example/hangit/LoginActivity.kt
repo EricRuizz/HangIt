529ee128316e8d9bade18bde850d2481712fd478
Eric <eric.ruiz@enti.cat>
2022-11-03 17:39:50 +0100

    Login and Splash screen working

--- app/src/main/java/com/example/hangit/LoginActivity.kt
+++ app/src/main/java/com/example/hangit/LoginActivity.kt
@@ -5,31 +5,29 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Toast
-import com.example.hangit.databinding.ActivityMainBinding
+import com.example.hangit.databinding.ActivityLoginBinding
 import kotlinx.coroutines.*
 
 class LoginActivity : AppCompatActivity() {
 
-    private lateinit var binding: ActivityMainBinding
+    private lateinit var binding: ActivityLoginBinding
     private val CORRECT_USERNAME = "Focalita"
     private val CORRECT_PASSWORD = "Focalita2"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        binding = ActivityMainBinding.inflate(layoutInflater)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        /*
         binding.loginButton.setOnClickListener {
             val username = binding.userInput.text.toString()
             val password = binding.passwordInput.text.toString()
 
             if(username == CORRECT_USERNAME && password == CORRECT_PASSWORD) {
-                //hem fet login
                 runBlocking {
                     CoroutineScope(Dispatchers.Default).launch {
-                        binding.progressBar.visibility = View.VISIBLE
+                       // binding.progressBar.visibility = View.VISIBLE
                         delay(3000)
 
                         val intent = Intent(this@LoginActivity, MainActivity::class.java)
@@ -43,6 +41,6 @@ class LoginActivity : AppCompatActivity() {
                 Toast.makeText(this, getString(R.string.error_password), Toast.LENGTH_SHORT).show()
             }
         }
-        */
+
     }
 }
--- app/src/main/java/com/example/hangit/SplashScreenActivity.kt
+++ app/src/main/java/com/example/hangit/SplashScreenActivity.kt
@@ -1,9 +1,29 @@
 package com.example.hangit
 
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import androidx.appcompat.app.AppCompatActivity
+import com.example.hangit.databinding.ActivityMainBinding
 import com.example.hangit.databinding.ActivitySplashScreenBinding
 
-class SplashScreenActivity {
+class SplashScreenActivity : AppCompatActivity() {
     private lateinit var binding : ActivitySplashScreenBinding
-    binding = ActivitySplashScreenBinding.inflate(layoutInflater)
-    setContentView(binding.root)
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivitySplashScreenBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        supportActionBar?.hide()
+
+        Handler().postDelayed(
+            {
+                val intent = Intent(this@SplashScreenActivity, LoginActivity::class.java)
+                startActivity(intent)
+                finish()
+            }, 3000)
+    }
+
 }
