8e5fa6da2890be800515dd899409001caaf59d4f
Focalita <marionabarranco@gmail.com>
2022-11-11 10:00:34 +0100

    Added firebase and new login

--- app/src/main/java/com/example/hangit/LoginActivity.kt
+++ app/src/main/java/com/example/hangit/LoginActivity.kt
@@ -6,13 +6,15 @@ import android.os.Bundle
 import android.view.View
 import android.widget.Toast
 import com.example.hangit.databinding.ActivityLoginBinding
+import com.google.firebase.auth.FederatedAuthProvider
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.OAuthProvider
 import kotlinx.coroutines.*
 
 class LoginActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityLoginBinding
-    private val CORRECT_USERNAME = "Focalita"
-    private val CORRECT_PASSWORD = "Focalita2"
+    private lateinit var firebaseAuth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,25 +22,23 @@ class LoginActivity : AppCompatActivity() {
         binding = ActivityLoginBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        firebaseAuth = FirebaseAuth.getInstance()
+
         binding.loginButton.setOnClickListener {
             val username = binding.userInput.text.toString()
             val password = binding.passwordInput.text.toString()
 
-            if(username == CORRECT_USERNAME && password == CORRECT_PASSWORD) {
-                runBlocking {
-                    CoroutineScope(Dispatchers.Default).launch {
-                        delay(3000)
+            firebaseAuth.signInWithEmailAndPassword(username, password).addOnSuccessListener {
+                val intent = Intent(this@LoginActivity, MainActivity::class.java)
+                startActivity(intent)
 
-                        val intent = Intent(this@LoginActivity, MainActivity::class.java)
-                        startActivity(intent)
+                finish()
 
-                        finish()
-                    }
-                }
+            }.addOnFailureListener {
 
-            } else {
                 Toast.makeText(this, getString(R.string.error_password), Toast.LENGTH_SHORT).show()
             }
+
         }
 
     }
