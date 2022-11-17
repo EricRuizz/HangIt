0d2a2fdd4b4e0afbce472c0a39b76e1b88eeda29
Roger <roger.aguilar@enti.cat>
2022-11-17 18:14:47 +0100

    continued with register
    ask about constrains and firebase

--- app/src/main/java/com/example/hangit/RegisterActivity.kt
+++ app/src/main/java/com/example/hangit/RegisterActivity.kt
@@ -1,7 +1,9 @@
 package com.example.hangit
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.example.hangit.databinding.ActivityLoginBinding
 import com.example.hangit.databinding.ActivityRegisterBinding
 import com.google.firebase.auth.FirebaseAuth
@@ -19,5 +21,41 @@ class RegisterActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         firebaseAuth = FirebaseAuth.getInstance()
+
+        binding.registerButton.setOnClickListener {
+            val username = binding.userInput.text.toString()
+            val password = binding.passwordInput.text.toString()
+            val password2 = binding.passwordInput.text.toString()
+
+            //TODO: mirar si l usuari existeix
+                if (password2 == password2) {
+                    firebaseAuth.createUserWithEmailAndPassword(username, password)
+                        .addOnSuccessListener {
+                            val intent = Intent(this@RegisterActivity, MainActivity::class.java)
+                            startActivity(intent)
+
+                            finish()
+
+                        }.addOnFailureListener {
+
+                            Toast.makeText(
+                                this,
+                                getString(R.string.error_password),
+                                Toast.LENGTH_SHORT
+                            )
+                                .show()
+                        }
+                } else {
+                    Toast.makeText(this, getString(R.string.register_error), Toast.LENGTH_SHORT)
+                        .show()
+                }
+
+        }
+        binding.buttongoToLogin.setOnClickListener {
+            val intent = Intent(this@RegisterActivity, LoginActivity::class.java)
+            startActivity(intent)
+
+            finish()
+        }
+        }
     }
-}
