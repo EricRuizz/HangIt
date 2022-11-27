5524394c6319eeeab38675e7e7288be97658a7f5
Focalita <marionabarranco@gmail.com>
2022-11-27 13:30:59 +0100

    continued settings

--- app/src/main/java/com/example/hangit/SettingsActivity.kt
+++ app/src/main/java/com/example/hangit/SettingsActivity.kt
@@ -3,14 +3,20 @@ package com.example.hangit
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.Toast
 import com.example.hangit.databinding.ActivityLoginBinding
 import com.example.hangit.databinding.ActivitySettingsBinding
+import com.google.firebase.FirebaseAppLifecycleListener
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
 
 class SettingsActivity : AppCompatActivity() {
     private lateinit var binding: ActivitySettingsBinding
     private lateinit var firebaseAuth: FirebaseAuth
+    private lateinit var firestore: FirebaseFirestore
+    private var audioOn: Boolean = true
+    private var notificationOn: Boolean = true
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,6 +25,12 @@ class SettingsActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         firebaseAuth = FirebaseAuth.getInstance()
+
+        firestore = FirebaseFirestore.getInstance()
+        FirebaseFirestore.setLoggingEnabled(true)
+
+        val collection = firestore.collection("userConfiguration")
+
 
         //Logout and go to login screen
         binding.logOutButton.setOnClickListener {
@@ -44,6 +56,60 @@ class SettingsActivity : AppCompatActivity() {
             startActivity(intent)
 
             finish()
+        }
+
+        //Activate or Deactivate audio
+        binding.audioButton.setOnClickListener {
+            if (!audioOn) {
+                audioOn = true
+                binding.audioTick.setVisibility(View.VISIBLE)
+                //Activate music
+
+
+            } else {
+                audioOn = false
+                binding.audioTick.setVisibility(View.GONE)
+                //Deactivate music
+            }
+
+            if (firebaseAuth.currentUser != null) {
+                //Save Configuration
+
+            } else {
+                //Can't save configuration, user is a guest
+                Toast.makeText(
+                    this,
+                    getString(R.string.can_not_save),
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
+        }
+
+        //Activate or Deactivate notifications
+        binding.notificationButton.setOnClickListener {
+            if (!notificationOn) {
+                notificationOn = true
+                binding.notificationsTick.setVisibility(View.VISIBLE)
+                //Activate notifications
+
+
+            } else {
+                notificationOn = false
+                binding.notificationsTick.setVisibility(View.GONE)
+                //Deactivate notifications
+            }
+
+            if (firebaseAuth.currentUser != null) {
+                //Save Configuration
+
+            } else {
+                //Can't save configuration, user is a guest
+                Toast.makeText(
+                    this,
+                    getString(R.string.can_not_save),
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
         }
     }
 
