db032db506c17f8d919b35b9def872f3b75e46b1
Focalita <marionabarranco@gmail.com>
2022-12-16 11:18:04 +0100

    fixed files

--- app/src/main/java/com/example/hangit/ranking/RankingViewModel.kt
+++ app/src/main/java/com/example/hangit/ranking/RankingViewModel.kt
@@ -4,16 +4,15 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.google.firebase.database.ktx.database
 import com.google.firebase.ktx.Firebase
-import com.example.hangit.ranking.
 
 class RankingViewModel : ViewModel() {
 
-    val ranking = MutableLiveData<RankingActivity>()
+   /* val ranking = MutableLiveData<RankingActivity>()
     val db = Firebase.database("https://hangit-660df-default-rtdb.europe-west1.firebasedatabase.app/")
         .getReference("ranking") //collection name
 
     fun openRanking(user: String)
     {
 
-    }
+    }*/
 }
